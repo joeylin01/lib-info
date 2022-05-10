2bbf913afaa234cbf20e0e1dee73d5473c146ffc
Joey Lin （林铭玺） <joey.lin@razer.com>
2022-05-10 15:42:54 +0800

    add bin

---
+++ bin/version.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+function run(argv) {
+  const execSync = require("child_process").execSync;
+  const fs = require("fs");
+
+  //切换路径
+  const commonPath = "cd " + argv[0];
+
+  const branchName = execSync(commonPath + `&git rev-parse --abbrev-ref HEAD`)
+    .toString()
+    .trim(); //当前的分支名字
+  const commitId = execSync(commonPath + "&git show -s --format=%H")
+    .toString()
+    .trim(); //当前版本号
+
+  const versionData = {
+    branch: branchName,
+    commit: commitId,
+  };
+
+  fs.writeFileSync("./common_version.json", JSON.stringify(versionData));
+}
+
+run(process.argv.slice(2));
